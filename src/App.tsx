5f49ebe87fd3f04e6f9cead90f71de20181be2b0
mGuv <mattmcg.gamedev@gmail.com>
2018-11-28 23:54:36 +0000

    Add initial react/typescript app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        Todo
+      </div>
+    );
+  }
+}
+
+export default App;
