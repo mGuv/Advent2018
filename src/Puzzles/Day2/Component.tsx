4093e35227a1eb5d6f8336afbcd0ab605a16554c
mGuv <mattmcg.gamedev@gmail.com>
2018-12-03 23:13:38 +0000

    Tidy day2

--- src/Puzzles/Day2/Component.tsx
+++ src/Puzzles/Day2/Component.tsx
@@ -28,17 +28,29 @@ class Component extends React.Component<{}, IState> {
     public render(): JSX.Element {
         return (
             <React.Fragment>
-                <div>This puzzle takes a series of strings and deals with analysing them for information before calculating a checksum</div>
-                <div>Difficulty: 2/10</div>
-                <div>The solution requires some basic string parsing, temporary storage of data.</div>
-                <div> Part A took 24 mins, Part B took 11 minutes</div>
+                <div>This puzzle takes a series of strings and deals with analysing them for information</div>
+                <div>The input is in the form of a series of "box ids" in the form of a string of characters</div>
+                <div>E.g. "abcaab"</div>
+                <div>Part 1</div>
+                <div>Difficulty: 1/10</div>
+                <div>To answer this part, each unique character in the id should be tallied and each ID must then state if it has a character than repeats exactly 2 times and if a character repeats exactly 3 times</div>
+                <div>This should be computed for every id and then finally multiplying the total number of IDs that had a match of 2 by the total number of ids that had a match of 3</div>
+                {this.state.submitted === true &&
+                <div>
+                    <p>Part 1 Answer: {this.state.answer1}</p>
+                </div>
+                }
+                <div>Part 2</div>
+                <div>Difficulty: 1/10</div>
+                <div>To answer this part, each ID must be compared against every other ID until they differ by exactly one character in one location</div>
+                <div>This is easiest to achieve by just comparing each unique pair and counting their differences, the instant a mismatch of 2 is found you can skip the current comparing id</div>
+                <div>If a comparison returns a difference of 1, you have found the correct IDs and can now figure out the matching characters</div>
                 <div><input onChange={this.updateInput}/>
                     <button onClick={this.submit}>Solve</button>
                 </div>
                 {this.state.submitted === true &&
                 <div>
-                    <p>Part A Answer: {this.state.answer1}</p>
-                    <p>Part B Answer: {this.state.answer2}</p>
+                    <p>Part 2 Answer: {this.state.answer2}</p>
                 </div> }
             </React.Fragment>
 
--- src/Puzzles/Day2/Part1/Solver.ts
+++ src/Puzzles/Day2/Part1/Solver.ts
@@ -1,29 +1,31 @@
+import BaseSolver from '../BaseSolver';
 import BoxScanner from './BoxScanner';
 import IScanResult from './IScanResult';
 
-class Solver
+class Solver extends BaseSolver
 {
     private scanner:BoxScanner;
 
     public constructor() {
+        super();
         this.scanner = new BoxScanner();
     }
 
     public Solve(input: string): number
     {
-        if(input.indexOf("\n")) {
-            input = input.replace("\n", " ");
-        }
+        const boxIds: string[] = this.ParseInput(input);
 
+        // Track how many had the same character 2 and/or 3 times
         let totalTwos = 0;
         let totalThrees = 0;
-
-        const boxIds: string[] = input.split(" ");
         boxIds.forEach((boxId: string) => {
+            // Scan each box id and add their result to the running total
             const scanResult:IScanResult = this.scanner.Scan(boxId);
             totalTwos += scanResult.two;
             totalThrees += scanResult.three;
         });
+
+        // Compute the hash by multiplying them
        return totalTwos * totalThrees;
     }
 }
